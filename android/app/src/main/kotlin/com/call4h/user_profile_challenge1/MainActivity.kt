afe0c9d9cf865bc850b36a08f4f01c99cc9599b8
abdullah <a77x86@gmail.com>
2019-06-10 16:06:46 +0400

    inital commit

---
+++ android/app/src/main/kotlin/com/call4h/user_profile_challenge1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.call4h.user_profile_challenge1
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
